0a70b706f0c6bbbcc7ee0c6fc6e39bda85c96530
Alexandr Borisov <aishek@gmail.com>
2011-10-19 15:57:26 +0400

    added recursively change parents statuses

--- lib/issue_observer_patch.rb
+++ lib/issue_observer_patch.rb
@@ -11,17 +11,24 @@ module IssueObserverPatch
     # If all child issues have status with id 5,
     # sets parent issue's status to id 11
     def after_update(issue)
-      if issue.status_id == 5 and issue.parent.present?
-        should_change_parent_status = true
-        issue.parent.children.each do |child|
-          should_change_parent_status = (child.status_id == 5)
-          break unless should_change_parent_status
-        end
-      
-        # there is one trouble with this statement:
-        # no emails sends on this update
-        Issue.update_all("status_id = 11", ["id = ?", issue.parent.id]) if should_change_parent_status
+      update_issue_status_if_children_closed(issue.parent) if issue.status_id == 5 and issue.parent.present?
+    end
+    
+    def update_issue_status_if_children_closed(issue)
+      return if issue.children.empty?
+
+      should_change_parent_status = true
+      issue.children.each do |child|
+        should_change_parent_status = (child.status_id == 5)
+        break unless should_change_parent_status
       end
+      
+      # there is one trouble with this statement:
+      # no emails sends on this update
+      Issue.update_all("status_id = 5", ["id = ?", issue.id]) if should_change_parent_status
+
+      # recursively updates parent
+      update_issue_status_if_children_closed(issue.parent) if issue.parent.present?
     end
   end    
 end
